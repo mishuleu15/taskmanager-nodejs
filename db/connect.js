54b897edf0eba980b235c2151bf285b7395daea5
unknown <mishu_leu15@yahoo.com>
2021-11-17 00:09:41 +0200

    fixed URI shown on repo

--- db/connect.js
+++ db/connect.js
@@ -4,6 +4,11 @@ const connectionString =
   'mongodb+srv://slick123:slick123@slickrobocode.362t2.mongodb.net/taskmanager?retryWrites=true&w=majority';
 
 mongoose
-  .connect(connectionString)
-  .then(() => console.log('Connected to the Db...'))
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to the DB...'))
   .catch((error) => console.log(error));

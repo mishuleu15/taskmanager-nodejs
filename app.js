e9c70fdd94e18f0bda4901bcf64cd2ccb1065fdd
unknown <mishu_leu15@yahoo.com>
2021-11-23 22:13:07 +0200

    added custom middlewares for error hadling and async functions

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const PORT = 3000;
 
 // middleware
-
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res, next) => {
-  res.status(200).send('Hello World');
-});
-
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
